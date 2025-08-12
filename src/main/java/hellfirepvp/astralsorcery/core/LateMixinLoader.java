f9b8a8858225cbf0eae60c3f92a9caf3924ac055
jack <jack>
2025-08-12 08:41:57 +0200

    Enabled mixins

---
+++ src/main/java/hellfirepvp/astralsorcery/core/LateMixinLoader.java
@@ -0,0 +1,30 @@
+package hellfirepvp.astralsorcery.core;
+
+import com.gtnewhorizon.gtnhmixins.ILateMixinLoader;
+import com.gtnewhorizon.gtnhmixins.LateMixin;
+import cpw.mods.fml.relauncher.FMLLaunchHandler;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@LateMixin
+@IFMLLoadingPlugin.MCVersion("1.7.10")
+public class LateMixinLoader  implements ILateMixinLoader {
+
+    @Override
+    public String getMixinConfig() {
+        return "mixins.astralsorcery.late.json";
+    }
+
+    @Override
+    public List<String> getMixins(Set<String> loadedCoreMods) {
+        final List<String> mixins = new ArrayList<>();
+        if (FMLLaunchHandler.side().isClient()) {
+            // mixins.add("tabbychat.MixinTabbyChat");
+            // Lorem ipsum
+        }
+        return mixins;
+    }
+}
